b5b9f277e6d9196fa2997116f85fe9bdb03afc73
Adam Kirschner <>
2019-05-10 17:34:27 -0500

    initial passport & google auth setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const googleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-})
+passport.use(new googleStrategy())
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
+
